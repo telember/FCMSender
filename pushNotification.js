b57ad5816fc10f1d5ff22fd2dc2bbb11b9018c1c
tylerlantern <tylerlantern@gmail.com>
2025-05-26 16:24:10 +0700

    fix script to run loop

--- pushNotification.js
+++ pushNotification.js
@@ -5,43 +5,61 @@ const { getAuthentication } = require('./auth');
 async function pushNotification() {
   const { access_token, project_id } = await getAuthentication();
   let input = require(path.join(__dirname, 'input.json'));
-  const { tokens, title, body } = input
+  const { tokens, title, body, time } = input
 
   for (const token of tokens) {
-    const payloadJSON = {
-      message: {
-        token: token,
-        data: {
-          voucherRedemption: "{\"placeId\":1716,\"placeName\":\"Prae Jecko Gecko Lover\",\"firstName\":\"Tyler\",\"lastName\":\"Lantern\",\"voucherId\":3220,\"status\":\"valid\",\"alertId\":32860,\"voucherCode\":\"VC-Y7D6-JZ42\",\"savedDate\":\"2024-12-11T11:11:47.000Z\",\"usedDate\":\"2024-12-12T04:26:33.000Z\",\"rejectedDate\":null,\"expiredDate\":\"2024-12-31T16:59:59.000Z\",\"voucherTitle\":\"Eat\",\"createdAt\":\"1733977594\"}"
-        },
-        notification: {
-          title: title,
-          body: body
-        },
-        apns: {
-          payload: {
-            aps: {
-              "mutable-content": 1,
-              "content-available": 1
+    for (let i = 0; i < time; i++) {
+      const payloadJSON = {
+        message: {
+          token: token,
+          data: {
+            voucherRedemption: JSON.stringify({
+              placeId: 1716,
+              placeName: "Prae Jecko Gecko Lover",
+              firstName: "Tyler",
+              lastName: "Lantern",
+              voucherId: 3220,
+              status: "valid",
+              alertId: 32860,
+              voucherCode: "VC-Y7D6-JZ42",
+              savedDate: "2024-12-11T11:11:47.000Z",
+              usedDate: "2024-12-12T04:26:33.000Z",
+              rejectedDate: null,
+              expiredDate: "2024-12-31T16:59:59.000Z",
+              voucherTitle: "Eat",
+              createdAt: "1733977594"
+            })
+          },
+          notification: {
+            title: `${title}+${i}`,
+            body: body
+          },
+          apns: {
+            payload: {
+              aps: {
+                "mutable-content": 1,
+                "content-available": 1
+              }
             }
           }
         }
-      }
-    }
-    console.log('#FCM:', JSON.stringify(payloadJSON));
-    const response = await fetch(`https://fcm.googleapis.com/v1/projects/${project_id}/messages:send`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${access_token}`
-      },
-      method: "POST",
-      body: JSON.stringify(payloadJSON),
-    });
-    const result = await response.json();
-    console.log('Push Notification Response:', result);
-    console.log('------------------------------------------------------------------------------')
-  }
+      };
 
+      console.log(`#FCM Send ${i + 1}/${time} for token ${token}`);
+      const response = await fetch(`https://fcm.googleapis.com/v1/projects/${project_id}/messages:send`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`
+        },
+        method: "POST",
+        body: JSON.stringify(payloadJSON),
+      });
+
+      const result = await response.json();
+      console.log('Push Notification Response:', result);
+      console.log('------------------------------------------------------------------------------');
+    }
+  }
 }
 
 module.exports = { pushNotification };
